6d32b087e9cc893d3330a195eacad7500fcccff1
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-12-08 15:30:03 +0300

    Add New and Create for Users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,6 +7,20 @@ defmodule Rumbl.UserController do
     render conn, "index.html", users: users
   end
 
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
+  end
+
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
     render conn, "show.html", user: user
