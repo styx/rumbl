a5bdf3d445c1643ccfc2ba1ee6d09fcaee063ba5
Mikhail S. Pobolovets <styx.mp@gmail.com>
2017-01-12 11:59:43 +0300

    Show errors on create

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -14,11 +14,15 @@ defmodule Rumbl.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
 
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
   end
 
   def show(conn, %{"id" => id}) do
