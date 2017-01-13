2babff26ac1e75d761e9da67eda0d24df4b76215
Mikhail S. Pobolovets <styx.mp@gmail.com>
2017-01-13 12:11:17 +0300

    Password processing

--- web/models/user.ex
+++ web/models/user.ex
@@ -16,4 +16,30 @@ defmodule Rumbl.User do
     |> validate_length(:username, min: 1, max: 30)
     |> unique_constraint(:username)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 50)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
